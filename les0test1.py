95d5cb483f9c240868202f80085e99886e97ebc6
PAvel <95528324+Pavel-L-40@users.noreply.github.com>
2024-08-29 12:50:16 +0300

    main file home_task

---
+++ les0test1.py
@@ -0,0 +1,13 @@
+# 1st prog
+print((9 ** 0.5) * 5)
+
+#2st prog
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3st prog
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == (2*(2+2)))
+
+#4st prog
+print(int(123.456 // 0.1 % 10))
